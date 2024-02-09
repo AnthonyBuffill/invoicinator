d4880fd6c377a29835e0e43ef94939214c9d8197
Luke Garnsey <lukegarnsey@gmail.com>
2024-02-09 16:30:24 -0500

    Tailwind setup, autoprefixer:postcss:postcss-cli:tailwindcss devDependancies added to package.json. Intial setup for main&home.handlbars, added main-navbar partial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["views/**/*.{html,js,handlebars}'"],
+  theme: {
+    colors:{
+      'main-1':'#6BA2FF',
+      'accent-1':'#440100',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
