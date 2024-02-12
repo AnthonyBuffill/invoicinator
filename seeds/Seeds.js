6fdf85db01fbdf9485a3fd83560ba24f90e7d74b
mwg396 <michael.graham1029@gmail.com>
2024-02-12 11:41:17 -0600

    corrected seeds data

--- seeds/Seeds.js
+++ seeds/Seeds.js
@@ -1,10 +1,10 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
 const { Invoice } = require('../models');
-// const { Comments } = require('../models')
+
 const userData = require('./userData.json');
 const invoice = require('./invoicesData.json');
-// const commentData = require('./Comments.json')
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
@@ -17,10 +17,7 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-//   await Comments.bulkCreate(commentData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
+
 
   process.exit(0);
 };
