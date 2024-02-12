e29851670ec3330f57389874e9d9cb8843f1c863
mwg396 <153116787+mwg396@users.noreply.github.com>
2024-02-12 13:50:41 -0600

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-<<<<<<< HEAD
 const dashboardRoutes = require ('./dashboardRoutes')
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
@@ -10,15 +9,3 @@ router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
-=======
-const apiRoutes = require('./apiRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
-
->>>>>>> c944fdb23398d07ee43cf732fa54ed412b7e15ad
