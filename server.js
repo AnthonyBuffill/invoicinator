24330339a6757011d704e459fc58b9865705f4e1
Luke Garnsey <lukegarnsey@gmail.com>
2024-02-12 12:57:02 -0500

    Added invoice dashboard page and invoice container

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ app.get("/", (req, res)=>{
 app.get("/user", (req, res)=>{
   res.render("login");
 })
+app.get("/dashboard", (req, res)=>{
+  res.render("dashboard");
+});
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
